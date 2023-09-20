c9b57da50330e11754d86798582323e75b33770a
Adrien MENU-GUILLEMIN <amenuguillemin@mantu.com>
2023-09-20 12:30:49 +0200

    clean couple of warnings

--- MailboxSearch/KeywordStatisticsSearchResult.cs
+++ MailboxSearch/KeywordStatisticsSearchResult.cs
@@ -25,29 +25,24 @@
 
 namespace Microsoft.Exchange.WebServices.Data
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+	/// <summary>
+	/// Represents the keyword statistics result.
+	/// </summary>
+	public sealed class KeywordStatisticsSearchResult
+	{
+		/// <summary>
+		/// Keyword string
+		/// </summary>
+		public string Keyword { get; set; }
 
-    /// <summary>
-    /// Represents the keyword statistics result.
-    /// </summary>
-    public sealed class KeywordStatisticsSearchResult
-    {
-        /// <summary>
-        /// Keyword string
-        /// </summary>
-        public string Keyword { get; set; }
+		/// <summary>
+		/// Number of item hits
+		/// </summary>
+		public int ItemHits { get; set; }
 
-        /// <summary>
-        /// Number of item hits
-        /// </summary>
-        public int ItemHits { get; set; }
-
-        /// <summary>
-        /// Total size
-        /// </summary>
-        [CLSCompliant(false)]
-        public ulong Size { get; set; }
-    }
+		/// <summary>
+		/// Total size
+		/// </summary>
+		public ulong Size { get; set; }
+	}
 }
