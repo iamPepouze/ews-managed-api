d00884690857d09bf99d9788c5a4861c436dcd03
Adrien MENU-GUILLEMIN <amenuguillemin@mantu.com>
2023-09-20 12:04:51 +0200

    migrate to net 6

--- Search/GetUserPhotoResults.cs
+++ Search/GetUserPhotoResults.cs
@@ -25,67 +25,65 @@
 
 namespace Microsoft.Exchange.WebServices.Data
 {
-    using System;
-    using System.Collections.ObjectModel;
-    using System.Drawing;
-    using System.IO;
-    using Microsoft.Exchange.WebServices.Data.Enumerations;
+	using System;
+	using System.Drawing;
+	using System.IO;
 
-    /// <summary>
-    /// Represents the results of a GetUserPhoto operation.
-    /// </summary>
-    public sealed class GetUserPhotoResults
-    {
-        /// <summary>
-        /// Creates a new instance of the <see cref="GetUserPhotoResults"/> class.
-        /// </summary>
-        internal GetUserPhotoResults()
-        {
-        }
+	/// <summary>
+	/// Represents the results of a GetUserPhoto operation.
+	/// </summary>
+	public sealed class GetUserPhotoResults
+	{
+		/// <summary>
+		/// Creates a new instance of the <see cref="GetUserPhotoResults"/> class.
+		/// </summary>
+		internal GetUserPhotoResults()
+		{
+		}
 
-        /// <summary>
-        /// Accessors for the picture data
-        /// </summary>
-        public byte[] Photo { get; internal set; }
+		/// <summary>
+		/// Accessors for the picture data
+		/// </summary>
+		public byte[] Photo { get; internal set; }
 
-        /// <summary>
-        /// Accessors for the Photo EntityTag
-        /// </summary>
-        public string EntityTag { get; internal set; }
+		/// <summary>
+		/// Accessors for the Photo EntityTag
+		/// </summary>
+		public string EntityTag { get; internal set; }
 
-        /// <summary>
-        /// Accessors for the ContentType of the photo
-        /// </summary>
-        public string ContentType { get; internal set; }
+		/// <summary>
+		/// Accessors for the ContentType of the photo
+		/// </summary>
+		public string ContentType { get; internal set; }
 
-        /// <summary>
-        /// Accessors for the Expries header tag
-        /// </summary>
-        public DateTime Expires { get; internal set; }
+		/// <summary>
+		/// Accessors for the Expries header tag
+		/// </summary>
+		public DateTime Expires { get; internal set; }
 
-        /// <summary>
-        /// The status of the photo response
-        /// </summary>
-        public GetUserPhotoStatus Status { get; internal set; }
+		/// <summary>
+		/// The status of the photo response
+		/// </summary>
+		public GetUserPhotoStatus Status { get; internal set; }
 
-        /// <summary>
-        /// Creates an image from the photo data
-        /// </summary>
-        /// <returns>The photo data as an Image</returns>
-        public Image AsImage()
-        {
-            if (this.Photo == null || this.Photo.Length == 0)
-            {
-                throw new InvalidOperationException("Cannot create image when no photo data returned.");
-            }
+		/// <summary>
+		/// Creates an image from the photo data
+		/// </summary>
+		/// <returns>The photo data as an Image</returns>
+		public Image AsImage()
+		{
+			if (this.Photo == null || this.Photo.Length == 0)
+			{
+				throw new InvalidOperationException("Cannot create image when no photo data returned.");
+			}
 
-            Image img;
-            using (MemoryStream stream = new MemoryStream(this.Photo))
-            {
-                img = Image.FromStream(stream);
-            }
+			Image img;
+			using (MemoryStream stream = new MemoryStream(this.Photo))
+			{
+				img = Image.FromStream(stream);
+			}
 
-            return img;
-        }
-    }
+			return img;
+		}
+	}
 }
